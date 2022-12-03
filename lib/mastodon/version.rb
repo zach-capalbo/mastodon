fc2b3e9c49103f64f42008e5fe55f0c4f8b39ea0
Zach Capalbo <zach.geek@gmail.com>
2022-12-02 19:42:51 -0500

    Color and logged in tweaks

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,7 +21,7 @@ module Mastodon
     end
 
     def suffix
-      ''
+      ' (with modifications)'
     end
 
     def to_a
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'zach-capalbo/mastodon')
     end
 
     def source_base_url
